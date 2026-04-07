93e5564024650a120dab68671c6cdb1851bc864c
Artem Milaev <artemik.milaev.2004@mail.ru>
2026-04-08 01:22:18 +0700

    Работает для множества тикеров

--- data/websocket_client.py
+++ data/websocket_client.py
@@ -2,6 +2,7 @@ import random
 import string
 import json
 import re
+import time
 from enum import Enum
 from typing import TypedDict
 from websocket import create_connection
@@ -23,9 +24,10 @@ class OHLC(TypedDict):
 
 class TradingViewWebSocket:
     def __init__(self):
-        self.ws = None
-        self.chart_session = None
-        self.quote_session = None
+        self._ws = None
+        self._chart_session = None
+        self._quote_session = None
+        self._i = 1
 
     def __enter__(self):
         self._connect()
@@ -33,21 +35,23 @@ class TradingViewWebSocket:
         return self
 
     def __exit__(self, exc_type, exc, tb):
-        self.ws.close()
+        self._ws.close()
         return False
 
     def get_historical_bars(
         self,
-        symbol: str = "BTCUSDT",
+        symbol: str = "BTCUSDT.P",
         timeframe: Timeframe = Timeframe.H1
     ) -> list[OHLC] | None:
         
         self._request_historical_data(symbol, timeframe)
+        self._i += 1
+
         msg = self._wait_for_historical_data()
         if not msg:
             return None
-        ohlc = self._extract_price_bars(msg)
-        return ohlc
+        
+        return self._extract_price_bars(msg)
 
     def _connect(self):
         headers = {
@@ -58,18 +62,12 @@ class TradingViewWebSocket:
             'Upgrade': 'websocket'
         }
 
-        self.ws = create_connection("wss://data.tradingview.com/socket.io/websocket", headers=headers)
+        self._ws = create_connection("wss://data.tradingview.com/socket.io/websocket", headers=headers)
 
     @staticmethod
-    def _generate_id(prefix):
+    def _generate_string_session(prefix):
         random_string = ''.join(random.choice(string.ascii_lowercase) for _ in range(12))
         return prefix + random_string
-    
-    def _generate_chart_session(self):
-        return self._generate_id("cs_")
-    
-    def _generate_quote_session(self):
-        return self._generate_id("qs_")
     
     @staticmethod
     def _create_message(func: str, param_list: list):
@@ -78,63 +76,71 @@ class TradingViewWebSocket:
     
     def _send_message(self, func: str, args: list):
         message = self._create_message(func, args)
-        self.ws.send(message)
+        self._ws.send(message)
 
     def _setup_sessions(self):
-        self.chart_session = self._generate_chart_session()
-        self.quote_session = self._generate_quote_session()
+        self._chart_session = self._generate_string_session("cs_")
+        self._quote_session = self._generate_string_session("qs_")
 
         self._send_message("set_auth_token", ["unauthorized_user_token"])
-        self._send_message("chart_create_session", [self.chart_session, ""])
-        self._send_message("quote_create_session", [self.quote_session])
-        self._send_message("quote_set_fields", [self.quote_session])
+        self._send_message("chart_create_session", [self._chart_session, ""])
+        self._send_message("quote_create_session", [self._quote_session])
+        self._send_message("quote_set_fields", [self._quote_session])
 
     def _request_historical_data(self, symbol: str, timeframe: Timeframe = Timeframe.H1):
         symbol_string = "=" + json.dumps({"symbol": f"BYBIT:{symbol}"})
 
-        self._send_message("resolve_symbol", [self.chart_session, "sds_sym_1", symbol_string])
-        self._send_message("create_series", [self.chart_session, "sds_1", "s1", "sds_sym_1", timeframe.value, 300, ""])
-        self._send_message("quote_add_symbols", [self.quote_session, f"BYBIT:{symbol}"])
+        self._send_message("resolve_symbol", [self._chart_session, f"sds_sym_{self._i}", symbol_string])
 
-    def _wait_for_historical_data(self) -> dict | None:
-        max_attempts = 6
+        if self._i > 1:
+            self._send_message(
+                "modify_series", 
+                [self._chart_session, "sds_1", f"s{self._i}", f"sds_sym_{self._i}", timeframe.value, ""]
+            )
+        else:
+            self._send_message(
+                "create_series", 
+                [self._chart_session, "sds_1", "s1", "sds_sym_1", timeframe.value, 300, ""]
+            )
 
-        for _ in range(max_attempts):
-            result = self.ws.recv()
+    def _wait_for_historical_data(self) -> list[dict] | None:
+        timeout = 3
+        start = time.time()
+
+        while time.time() - start < timeout:
+            result = self._ws.recv()
             data = re.split(r"~m~\d+~m~", result)
-
             data = [json.loads(part) for part in data if part]
 
             for msg in data:
-                if msg.get("m") == "timescale_update":
-                    return msg
+                if msg.get("m") != "timescale_update":
+                    continue
+
+                series_dict = msg["p"][1]
+                for series_data in series_dict.values():
+                    if series_data.get("s"):
+                        return series_data.get("s")
+                
         return None
 
     @staticmethod
-    def _extract_price_bars(msg: dict) -> list[OHLC] | None:
-        series_dict = msg["p"][1]
+    def _extract_price_bars(s_list: list[dict]) -> list[OHLC]:
+        ohlc: list[OHLC] = [
+            {
+                "open": float(item["v"][1]),
+                "high": float(item["v"][2]),
+                "low": float(item["v"][3]),
+                "close": float(item["v"][4])
+            }
+            for item in s_list
+        ]
 
-        for series_data in series_dict.values():
-            s_list = series_data.get("s", [])
+        return ohlc
 
-            ohlc: list[OHLC] = [
-                {
-                    "open": float(item["v"][1]),
-                    "high": float(item["v"][2]),
-                    "low": float(item["v"][3]),
-                    "close": float(item["v"][4])
-                }
-                for item in s_list
-            ]
-
-            if ohlc:
-                return ohlc
-        return None
-
-with TradingViewWebSocket() as ws:
-    symbols = ["BTCUSDT", "ETHUSDT", "SOLUSDT", "HYPEUSDT"]
-    for symbol in symbols:
-        ohlc = ws.get_historical_bars(symbol)
-        with open(f"{symbol}.json", "w", encoding="utf-8") as f:
-            json.dump(ohlc, f, indent=4, ensure_ascii=False)
-
+if __name__ == "__main__":
+    with TradingViewWebSocket() as ws:
+        symbols = ["BTCUSDT.P", "ETHUSDT.P", "SOLUSDT.P", "HYPEUSDT.P"]
+        for symbol in symbols:
+            ohlc = ws.get_historical_bars(symbol)
+            with open(f"{symbol}.json", "w", encoding="utf-8") as f:
+                json.dump(ohlc, f, indent=4, ensure_ascii=False)
