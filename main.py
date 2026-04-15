d962a94135bbc6032e199b3c320ffde5e9d1c269
Artem Milaev <artemik.milaev.2004@mail.ru>
2026-04-16 01:54:43 +0700

    Реализовано точка входа

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-import time
+import asyncio
+import signal
+import logging
 
-from data.market_data import MarketDataClient
 from config import setup_logging
+from core.startup_updater import StartupUpdater
+from core.scheduler import Scheduler
+
+logger = logging.getLogger(__name__)
 
 
-def main():
+async def main():
     setup_logging()
-    market_client = MarketDataClient()
-    start = time.time()
-    data = market_client.get_all_historical_ohlc()
-    print(f"Затрачено: {time.time() - start}")
+
+    stop_event = asyncio.Event()
+
+    scheduler = Scheduler()
+
+    def shutdown():
+        logger.info("Получен сигнал завершения...")
+        scheduler.shutdown()
+        stop_event.set()
+
+    loop = asyncio.get_running_loop()
+
+    loop.add_signal_handler(signal.SIGINT, shutdown)
+    loop.add_signal_handler(signal.SIGTERM, shutdown)
+
+    startup_updater = StartupUpdater()
+    await startup_updater.run()
+
+    scheduler.start()
+
+    logger.info("Приложение запущено и работает...")
+
+    await stop_event.wait()
+
+    logger.info("Приложение завершено корректно.")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
