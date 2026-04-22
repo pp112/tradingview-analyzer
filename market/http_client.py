6d120fd5c39ec31a9dfb8dc9d0cf87df2c4c549f
Artem Milaev <artemik.milaev.2004@mail.ru>
2026-04-23 01:16:20 +0700

    Добавлены изменения цен и объемов в http запрос

--- market/http_client.py
+++ market/http_client.py
@@ -1,5 +1,6 @@
 import json
 import aiohttp
+import copy
 
 from config import get_logger
 
@@ -10,6 +11,9 @@ class TradingViewHttpClient:
     """
     HTTP клиент для получения списка доступных тикеров с TradingView.
     """
+
+    URL = "https://scanner.tradingview.com/crypto/scan?label-product=popup-screener-crypto-cex"
+
     def __init__(self):
         with open("market/payloads/scanner.json", encoding="utf-8") as f:
             self.payload = json.load(f)
@@ -21,27 +25,43 @@ class TradingViewHttpClient:
             "Referer": "https://www.tradingview.com/"
         }
 
-    async def fetch_all_tickers(self) -> list[str]:
+    async def fetch_data(self) -> dict[str, dict[str, float]]:
         """
-        Возвращает список всех доступных крипто-тикеров с TradingView (ByBit).
+        Возвращает список всех доступных крипто-тикеров .
         """
-        url = "https://scanner.tradingview.com/crypto/scan?label-product=popup-screener-crypto-cex"
+        data = await self._post(self.payload)
+        parsed_data = self._parse_data(data)
+        logger.info(f"Получено тикеров с данными: {len(parsed_data)}")
+        return parsed_data
 
+    async def _post(self, payload: dict) -> dict:
+        """
+        Выполняет POST-запрос к TradingView Scanner и возвращает сырой JSON.
+        """
         async with aiohttp.ClientSession() as session:
-            async with session.post(url, headers=self.headers, json=self.payload) as r:
+            async with session.post(self.URL, headers=self.headers, json=payload) as r:
                 r.raise_for_status()
-                data = await r.json()
+                return await r.json()
 
-        tickers = [
-            d.get("s").split(":")[1] 
-            for d in data["data"]
-        ]
-
-        logger.info(f"Успешно получено тикеров: {len(tickers)}")
-        return tickers
+    def _parse_data(self, data: dict) -> dict[str, dict[str, float]]:
+        """
+        Парсер json-ответа post-запроса  
+        """
+        result = {}
+        for item in data["data"]:
+            symbol_name = item["s"].split(":")[1]
+            change_price = round(item["d"][0], 2)
+            change_volume = round(item["d"][1], 2)
+            result[symbol_name] = {
+                "change_price": change_price,
+                "change_volume": change_volume
+            }
+        return result
 
 
 if __name__ == "__main__":
     import asyncio
-    tickers = asyncio.run(TradingViewHttpClient().fetch_all_tickers())
-    print(tickers)
+    http_client = TradingViewHttpClient()
+    result = asyncio.run(http_client.fetch_data())
+    with open("market/rersponse.json", "w", encoding="utf-8") as f:
+        json.dump(result, f, indent=4, ensure_ascii=False)
